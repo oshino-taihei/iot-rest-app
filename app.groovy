5219dc022349846f6f9de3793fcb0c6b03f95027
oshino-taihei <oshino.taihei+github@gmail.com>
2017-01-19 11:33:10 +0900

    メッセージ表示、リセットできるようにした

--- app.groovy
+++ app.groovy
@@ -1,10 +1,40 @@
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.concurrent.CopyOnWriteArrayList
+import java.time.*
 
-@RestController
+@Controller
 class RestApp {
-    @RequestMapping("/")
-    String home() {
-      "hello,world"
+  private static final Logger logger = LoggerFactory.getLogger(RestApp.class)
+  private static List<Map> dataList = new CopyOnWriteArrayList<>()
+
+  @RequestMapping(value = "/")
+  public String home() {
+    "redirect:/messages"
+  }
+  @RequestMapping(value = "/messages", method = RequestMethod.GET)
+  @ResponseBody
+  public String index() {
+    render('index')
+  }
+  @RequestMapping(value = "/messages", method = RequestMethod.POST)
+  @ResponseBody
+  public String create(@RequestParam(required = false) String value) {
+    if (value != null) {
+      dataList.add([date:LocalDateTime.now(), value:value])
     }
+  }
+  @RequestMapping(value = "/messages/clear")
+  public String clear() {
+    dataList.clear()
+    "redirect:/messages"
+  }
+
+  private String render(String templateName) {
+    def f = new File("view/${templateName}.template")
+    def engine = new groovy.text.SimpleTemplateEngine()
+    def binding = ['dataList': dataList]
+    def template = engine.createTemplate(f).make(binding)
+    template.toString()
+  }
 }
